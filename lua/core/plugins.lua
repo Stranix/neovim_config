b3b31e54fe723569617b0bc61923f677c1cf6620
Stranix <Phantom2525@gmail.com>
2023-11-09 22:20:39 +0300

    Add plug telescope and toggleterm

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -14,6 +14,7 @@ require("lazy").setup({
   {"phaazon/hop.nvim"},
   {"neovim/nvim-lspconfig"},
   {"nvim-treesitter/nvim-treesitter"},
+  {"nvim-telescope/telescope.nvim"},
   {"navarasu/onedark.nvim"},
   {"hrsh7th/cmp-nvim-lsp"},
   {"hrsh7th/cmp-buffer"}, 
@@ -21,6 +22,7 @@ require("lazy").setup({
   {"hrsh7th/cmp-cmdline"}, 
   {"hrsh7th/nvim-cmp"},
   {"williamboman/mason.nvim"},
+  {'akinsho/toggleterm.nvim', vrsion = "*", config = true},
   {'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'},
   {'nvim-lualine/lualine.nvim', dependencies = 'nvim-tree/nvim-web-devicons'},
   {
