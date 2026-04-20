88846f4c17298e4c2ba9a0f7b33c3421dc360e51
Konstantin <Phantom2525@gmail.com>
2026-04-20 17:26:09 +0300

    Update treesitter.lua
    fix for v0.11

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,8 +1,12 @@
-require 'nvim-treesitter.configs'.setup {
-  ensure_installed = { "python", "lua", "htmldjango", "html", "json", "ini", 
-	"typescript", "tsx", "javascript"},
-  
-  sync_intsall = false,
+local status, ts = pcall(require, "nvim-treesitter.configs")
+if not status then
+    return
+end
+
+ts.setup {
+  ensure_installed = { "python", "lua", "htmldjango", "html", "json", "ini",
+        "typescript", "tsx", "javascript"},
+  sync_install = false, -- исправлено
   auto_install = true,
   highlight = {enable = true,}
 }
